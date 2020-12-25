7cf2965bd5b3be4b068dbec84a912edf55bae10d
frano-eo <66615541+frano-eo@users.noreply.github.com>
2020-12-25 12:22:05 +0200

    Update rps.js

--- FL/js/rps.js
+++ FL/js/rps.js
@@ -1,5 +1,5 @@
-document.getElementById("komBildo").src="img/p(3).png";
-document.getElementById("miaBildo").src="img/p(3).png";
+document.getElementById("komBildo").src= img/p(3).png;
+document.getElementById("miaBildo").src= img/p(3).png;
 document.getElementById("homPoento").innerHTML=0;
 document.getElementById("komPoento").innerHTML=0;
 document.getElementById("Ŝtono"  ).onclick = () => kompari(0);
